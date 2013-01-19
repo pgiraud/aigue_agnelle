333e5d194163dbcc4ab5030d947c3287f63cfae5
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2013-01-20 00:14:03 +0100

    Delay the photos loading

--- javascripts/script.js
+++ javascripts/script.js
@@ -28,14 +28,58 @@ $('.nav li a').click(function(){
 
     return false;
 });
-$('#photos_gallery').slides({
-    preload: true,
-    preloadImage: 'images/loading.gif',
-    effect: 'slide, fade',
-    crossfade: true,
-    slideSpeed: 350,
-    fadeSpeed: 500,
-    generateNextPrev: true,
-    generatePagination: false
+
+$().ready(function() {
+    $.each([
+        "http://farm9.staticflickr.com/8362/8395048113_d44e1c6e33_d.jpg",
+        "http://farm9.staticflickr.com/8093/8395050653_a468e5122d_d.jpg",
+        "http://farm9.staticflickr.com/8505/8396131284_c65b3af379_d.jpg",
+        "http://farm9.staticflickr.com/8195/8396131286_55453cd0ed_d.jpg",
+        "http://farm9.staticflickr.com/8230/8396131210_57ed33582d_d.jpg",
+        "http://farm9.staticflickr.com/8371/8396131234_81f3dba1b3_d.jpg",
+        "http://farm9.staticflickr.com/8214/8396131322_f5c4bdedd2_d.jpg",
+        "http://farm9.staticflickr.com/8089/8395048095_6583049308_d.jpg",
+        "http://farm9.staticflickr.com/8052/8395048103_e6dc6c1577_d.jpg"
+    ], function(index, url) {
+        console.log($('.slides_container'));
+        $('.slides_container').append($('<a />').append($('<img />', {
+                src: url,
+                width: 450
+            }) 
+        ));
+    });
+    $.each([
+        "http://farm9.staticflickr.com/8362/8395048113_d44e1c6e33_s_d.jpg",
+        "http://farm9.staticflickr.com/8093/8395050653_a468e5122d_s_d.jpg",
+        "http://farm9.staticflickr.com/8505/8396131284_c65b3af379_s_d.jpg",
+        "http://farm9.staticflickr.com/8195/8396131286_55453cd0ed_s_d.jpg",
+        "http://farm9.staticflickr.com/8230/8396131210_57ed33582d_s_d.jpg",
+        "http://farm9.staticflickr.com/8371/8396131234_81f3dba1b3_s_d.jpg",
+        "http://farm9.staticflickr.com/8214/8396131322_f5c4bdedd2_s_d.jpg",
+        "http://farm9.staticflickr.com/8089/8395048095_6583049308_s_d.jpg",
+        "http://farm9.staticflickr.com/8052/8395048103_e6dc6c1577_s_d.jpg"
+    ], function(index, url) {
+        $('.pagination')
+            .append(
+                $('<li />').append(
+                    $('<a />').append($('<img />', {
+                        src: url,
+                        width: 75 
+                    }))
+                )
+            );
+        
+    });
+    $('#photos_gallery').slides({
+        preload: true,
+        preloadImage: 'images/loading.gif',
+        effect: 'slide, fade',
+        crossfade: true,
+        slideSpeed: 350,
+        fadeSpeed: 500,
+        generateNextPrev: true,
+        generatePagination: false
+    });
+    $('body').scrollspy();
+
 });
-$('body').scrollspy();
