c928954c01eaa8e2ab300f261b0284283ce47c67
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2013-01-19 17:27:21 +0100

    Animated scrolling

--- javascripts/script.js
+++ javascripts/script.js
@@ -9,3 +9,23 @@ $(window).scroll(function(){
         $('.hidden-header').fadeOut();
     }
 });
+
+function scrollToDiv(element,navheight) {
+    var offset = element.offset();
+    var offsetTop = offset.top;
+    var totalScroll = offsetTop-navheight;
+
+    $('body,html').animate({
+        scrollTop: totalScroll
+    }, 600);
+}
+$('.nav li a').click(function(){
+
+    var el = $(this).attr('href');
+    var elWrapped = $(el);
+
+    scrollToDiv(elWrapped,5);
+
+    return false;
+});
+
